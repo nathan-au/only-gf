d737037b1872153bba6c7fe3556cd9699864f2e7
nathan-au <im.nathanau@gmail.com>
2025-10-28 21:29:38 -0400

    Photo library access, asset fetching, image display

--- OnlyGF/ContentView.swift
+++ OnlyGF/ContentView.swift
@@ -6,19 +6,77 @@
 //
 
 import SwiftUI
+import Photos
 
 struct ContentView: View {
+    @State private var firstImage: UIImage? = nil
     var body: some View {
         VStack {
             Image(systemName: "heart.circle")
                 .imageScale(.large)
                 .foregroundStyle(.blue)
             Text("Welcome to OnlyGF")
+            Button("Request Photo Access") {
+                requestPhotoAccess()
+            }
+            
+            if let img = firstImage {
+                Image(uiImage: img)
+
+            }
+
         }
         .padding()
     }
+    
+    func fetchPhotos() {
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.fetchLimit = 1
+        
+        let fetchResults = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+        let imageManager = PHImageManager.default()
+        guard let asset = fetchResults.firstObject else {
+            return
+        }
+        imageManager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, info in
+            
+            if let img = image {
+                self.firstImage = img
+
+            }
+                
+        }
+    }
+    
+    func requestPhotoAccess() {
+        let authStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+        
+        switch (authStatus) {
+        case .notDetermined:
+            print("request photo access")
+            PHPhotoLibrary.requestAuthorization(for: .readWrite) { reqStatus in
+                    print(reqStatus)
+            }
+        case .restricted:
+            print("sorry this app wont work")
+        case .denied:
+            print("please change app permissions")
+        case .authorized:
+            print("thanks for sharing your photos")
+            fetchPhotos()
+        case .limited:
+            print("please change app permission to full access")
+        default:
+            print("idk bro")
+        }
+    }
+    
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+
